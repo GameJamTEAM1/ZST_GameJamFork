82cdf6277d6c54a0409f9b60fb2597d6d0b60680
nyaanon <kubam554@gmail.com>
2023-12-17 05:46:08 +0100

    Added the missing scene

--- Assets/Src/Scripts/UI/ListPartyMembers.cs
+++ Assets/Src/Scripts/UI/ListPartyMembers.cs
@@ -20,10 +20,7 @@ public class ListPartyMembers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Add(text);
-        }
+
     }
 
     public void Add(GameObject element)
